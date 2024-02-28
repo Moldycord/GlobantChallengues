c6c140997dc3dce57f5ec8f9a7f8a1dcea373388
Danieer Galvez <Moldycord@gmail.com>
2024-02-27 22:47:51 -0500

    Finished algotrithm
    First i trie to clasificate each char  and separate them into three diferent lists that each one will contain a char type.
    Then i sort the letter and the number char lists to merge them into a new sorted list and print the result

---
+++ src/Main.kt
@@ -0,0 +1,7 @@
+import sorting.SortingStringAlgo
+
+fun main() {
+    val sortingStringAlgo = SortingStringAlgo()
+    sortingStringAlgo.sortString("231i49i23rifjdsijkkq0101o0oodpscl")
+    sortingStringAlgo.sortString("P@rAngar1cu÷iRImîçūąřoʻ")
+}
---
+++ src/sorting/SortingStringAlgo.kt
@@ -0,0 +1,41 @@
+package sorting
+
+class SortingStringAlgo {
+
+    fun sortString(inputString: String) {
+        val numberChars = mutableListOf<Pair<Char, Int>>()
+        val otherChars = mutableListOf<Char>()
+        val lettersChars = mutableListOf<Pair<Char, Int>>()
+
+
+        /**Iterate over the string and validate if each char is a letter, number or another type
+         * if is not any of those, is added to another list that will contain each char that is not a number
+         * or a letter
+         * **/
+
+        for (char in inputString) {
+            when {
+                char.isLetter() -> {
+                    val letterPair = Pair(char, if (char.isUpperCase()) 1 else 0)
+                    lettersChars.add(letterPair)
+                }
+
+                char.isDigit() -> {
+                    val digitPair = Pair(char, if (char.toString().toInt() % 2 == 0) 1 else 0)
+                    numberChars.add(digitPair)
+                }
+
+                else -> otherChars.add(char)
+            }
+        }
+
+        lettersChars.sortWith(compareBy({ it.second }, { it.first }))
+        numberChars.sortWith(compareBy({ it.second }, { it.first }))
+
+        val sortedChars = mutableListOf<Char>()
+        sortedChars.addAll(lettersChars.map { it.first })
+        sortedChars.addAll(numberChars.map { it.first })
+        sortedChars.addAll(otherChars)
+        println(sortedChars.joinToString(""))
+    }
+}
